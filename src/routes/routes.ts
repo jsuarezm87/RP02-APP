f8c88a557f13efd44fd23c1542d7e7cffd45f173
Jhonnatan Suarez Monzon <62630611+jsuarezm87@users.noreply.github.com>
2023-03-31 11:14:47 -0500

    Revert "Merge branch 'lazyload'"
    This reverts commit a41a6968306dac9f140ec451e44fb5c88cc8387d, reversing
    changes made to a66bf8912c27fae5bc52e008d4001e4419163d5d.

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 import { LazyPage1 } from '../01-lazyload/pages/LazyPage1';
 import { LazyPage2 } from '../01-lazyload/pages/LazyPage2';
 import { LazyPage3 } from '../01-lazyload/pages/LazyPage3';
-=======
-import { lazy, LazyExoticComponent } from 'react';
-// import { LazyPage1 } from '../01-lazyload/pages/LazyPage1';
-
-
-type JSXComponent = () => JSX.Element;
->>>>>>> parent of bce8d28 (Update routes.ts)
 
 interface Route {    
     to: string,
